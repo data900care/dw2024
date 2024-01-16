10ebcd9e4fc2d8d78afa38b3112bdfadceba29cc
Camille Capoul <data@900.care>
2024-01-16 10:36:32 +0000

    corrections

--- models/1_staging/shopify/stg_shopify__orders.sql
+++ models/1_staging/shopify/stg_shopify__orders.sql
@@ -17,14 +17,9 @@ renamed as (
         created_at as createdAt,
         currency,
         current_subtotal_price,
-        current_subtotal_price_set,
         current_total_discounts,
-        current_total_discounts_set,
-        current_total_duties_set,
         current_total_price,
-        current_total_price_set,
         current_total_tax,
-        current_total_tax_set,
         customer_id as shopify_customerId,
         name as orderName,
         number,
@@ -32,18 +27,12 @@ renamed as (
         processed_at,
         shipping_address_country,
         subtotal_price,
-        subtotal_price_set,
         taxes_included,
         test,
         total_discounts,
-        total_discounts_set,
         total_line_items_price,
-        total_line_items_price_set,
         total_price,
-        total_price_set,
-        total_shipping_price_set,
-        total_tax,
-        total_tax_set
+        total_tax
 
     from source
 
