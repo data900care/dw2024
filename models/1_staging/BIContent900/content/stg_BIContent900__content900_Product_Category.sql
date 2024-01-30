f5eb603cdb13bc5f011492400e46ff926a3d3c35
Camille Capoul <data@900.care>
2024-01-30 11:01:41 +0000

    impact_on_recurring_order_basket_size

--- models/1_staging/BIContent900/content/stg_BIContent900__content900_Product_Category.sql
+++ models/1_staging/BIContent900/content/stg_BIContent900__content900_Product_Category.sql
@@ -13,7 +13,8 @@ renamed as (
         category,
         cast(replace(coeff_market_vs_900_product_size,',','.') as numeric) as coeffMarketSize,
         cast(replace(water_consumption_market_product,',','.') as numeric) as equivalentProductWater,
-        cast(weight_of_plastic_market_product as int) as equivalentProductPlasticWeight
+        cast(weight_of_plastic_market_product as int) as equivalentProductPlasticWeight,
+        impact_on_recurring_order_basket_size
 
     from source
 
