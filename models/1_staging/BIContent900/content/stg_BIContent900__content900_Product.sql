100f9619cd180b0d001e3a6a8e6815ba52f972a2
Camille Capoul <data@900.care>
2024-01-31 12:35:18 +0000

    bundle columns removed from product

--- models/1_staging/BIContent900/content/stg_BIContent900__content900_Product.sql
+++ models/1_staging/BIContent900/content/stg_BIContent900__content900_Product.sql
@@ -16,10 +16,7 @@ renamed as (
         category as productCategory,
         trial_kit as trialKit,        
         case when lower(trial_kit) = lower("Trial Kit") then true else false end as isTrialKit,
-        cast(kit_home as bool) as kitHome,
-        cast(kit_kids as bool) as kitKids,
-        cast(kit_solid as bool) as kitSolid,
-        cast(kit_essentials as bool) as kitEssentials,                
+             
         cast(quantity_refill as int) as quantityRefill,
         
         cast(weight_of_plastic as int) as  weightPlastic,
