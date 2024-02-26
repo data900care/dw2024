5d10ead39ccd75ac4210fd888c905c200a6c0f57
Camille Capoul <data@900.care>
2024-02-26 13:18:57 +0000

    upsell Cancellation in invalid labels

--- models/11_init/init_invalidOrCancelledShopifyOrders.sql
+++ models/11_init/init_invalidOrCancelledShopifyOrders.sql
@@ -1,4 +1,18 @@
-select shopify_orderId , concat('cancel: ' , cancelReason) as invalidlabel from {{ ref("stg_shopify__order") }}
-where cancelled is true
+with upsellCancellation as 
+    (select shopify_orderId ,  concat('upsell:' , upsellType) as invalidlabel 
+    from {{ ref("stg_shopify__order")}} 
+    join {{ ref('stg_airtable_upsell') }} on  orderName = originalOrderName
+    where cancelled is true 
+    ),
+
+otherCancellation as 
+    (select shopify_orderId , concat('cancel: ' , cancelReason) as invalidlabel 
+    from {{ ref("stg_shopify__order") }}
+    where cancelled is true 
+    and shopify_orderId not in (select shopify_orderId from upsellCancellation))
+
+select shopify_orderId, invalidlabel from  upsellCancellation
+union all
+select shopify_orderId, invalidlabel from  otherCancellation
 union all
 select  shopify_orderId , invalidlabel from {{ ref("init_invalidShopifyOrders") }}
