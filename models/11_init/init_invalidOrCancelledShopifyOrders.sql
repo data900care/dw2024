391d5c3518c9f8dc2453cb7274b153ffded05b68
Camille Capoul <data@900.care>
2024-02-26 13:28:32 +0000

    unique InvalidLabel

--- models/11_init/init_invalidOrCancelledShopifyOrders.sql
+++ models/11_init/init_invalidOrCancelledShopifyOrders.sql
@@ -1,18 +1,25 @@
 with upsellCancellation as 
-    (select shopify_orderId ,  concat('upsell:' , upsellType) as invalidlabel 
+    (select shopify_orderId ,  concat('upsell:' , upsellType) as invalidLabel 
     from {{ ref("stg_shopify__order")}} 
     join {{ ref('stg_airtable_upsell') }} on  orderName = originalOrderName
     where cancelled is true 
     ),
 
 otherCancellation as 
-    (select shopify_orderId , concat('cancel: ' , cancelReason) as invalidlabel 
+    (select shopify_orderId , concat('cancel: ' , cancelReason) as invalidLabel 
     from {{ ref("stg_shopify__order") }}
     where cancelled is true 
-    and shopify_orderId not in (select shopify_orderId from upsellCancellation))
+    and shopify_orderId not in (select shopify_orderId from upsellCancellation)),
 
-select shopify_orderId, invalidlabel from  upsellCancellation
+fullList as 
+(
+select shopify_orderId, invalidLabel from  upsellCancellation
 union all
-select shopify_orderId, invalidlabel from  otherCancellation
+select shopify_orderId, invalidLabel from  otherCancellation
 union all
-select  shopify_orderId , invalidlabel from {{ ref("init_invalidShopifyOrders") }}
+select  shopify_orderId , invalidLabel from {{ ref("init_invalidShopifyOrders") }}
+)
+
+select shopify_orderId , max(invalidLabel) as invalidLabel
+from fullList
+group by shopify_orderId
