da3f139155be3646a1b3f1cde4d1d1a4da09030f
Camille Capoul <data@900.care>
2024-02-26 13:06:31 +0000

    cancel reason

--- models/11_init/init_invalidOrCancelledShopifyOrders.sql
+++ models/11_init/init_invalidOrCancelledShopifyOrders.sql
@@ -1,4 +1,4 @@
-select shopify_orderId , cancelReason as invalidlabel from {{ ref("stg_shopify__order") }}
+select shopify_orderId , concat('cancel: ' , cancelReason) as invalidlabel from {{ ref("stg_shopify__order") }}
 where cancelled is true
 union all
 select  shopify_orderId , invalidlabel from {{ ref("init_invalidShopifyOrders") }}
