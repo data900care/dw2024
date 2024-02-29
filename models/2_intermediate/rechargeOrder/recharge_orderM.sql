2ff75cfc4b32f6f25841f7f6428d7b40c2c9590b
Camille Capoul <data@900.care>
2024-02-29 09:49:18 +0000

    basket sizes on recharge side

--- models/2_intermediate/rechargeOrder/recharge_orderM.sql
+++ models/2_intermediate/rechargeOrder/recharge_orderM.sql
@@ -1,12 +1,19 @@
-select ro.*,
+with basketSize as
+(select recharge_orderId , sum(lineBasketSize)  as orderBasketSize
+from {{ ref('recharge_orderLineM') }}
+group by recharge_orderId
+)
+
+select ro.*,b.orderBasketSize,
 subscriptionsActiveCount,subscriptionsCount,firstSubscriptionDate,lastSubscriptionCancelledAt, maxsubscriptionOrderCount,
 minOrderIntervalFrequency,
 maxOrderIntervalFrequency
-
-,so.validOrder 
+,so.validOrder , so.customerOrderRank as shopify_customerOrderRank, so.orderCustomerType as shopify_orderCustomerType
 from {{ ref('inner_recharge_order') }} ro 
-left join {{ ref('recharge_orderSubscriptionSummary') }} s 
-using(recharge_orderId) 
+    left join basketSize b
+        using(recharge_orderId)
+    left join {{ ref('recharge_orderSubscriptionSummary') }} s 
+        using(recharge_orderId) 
 left join
-    {{ ref("inner_shopify__order") }} so
+    {{ ref("shopifyOrderL") }} so
     using(shopify_orderid )
