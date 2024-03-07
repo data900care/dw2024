1ec0e525e760d33120be6822179e1a7f961eb9ff
Camille Capoul <data@900.care>
2024-03-07 10:01:08 +0000

    OKR data stat

--- models/3_serve/stats/externalDataStats.sql
+++ models/3_serve/stats/externalDataStats.sql
@@ -1,3 +1,5 @@
 select 'grapevine' as dateSource,max(createdAt) as lastUpdate from {{ ref('stg_externalData__externalData_Grapevine_Survey_Data') }}
 union all
-select 'subscription' as dateSource, max(createdAt) as lastUpdate from {{ ref('stg_recharge__subscription') }}
+select 'subscription' as dateSource, max(createdAt) as lastUpdate from {{ ref('stg_recharge__subscription') }} 
+union all
+select 'OKR' as dateSource, max(weekEnd) as lastUpdate from {{ ref('stg_externalData__recharge_okr_CumSubscriptionsWeekly') }} 
