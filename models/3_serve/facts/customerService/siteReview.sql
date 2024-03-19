502a4f145d8c92a24d7a5bee0a10bd559aabe1ca
Camille Capoul <data@900.care>
2024-03-19 14:02:07 +0000

    retently

--- models/3_serve/facts/customerService/siteReview.sql
+++ models/3_serve/facts/customerService/siteReview.sql
@@ -1,1 +1,14 @@
-select * from {{ ref('unifiedSiteReviews') }}
+select createdAt,
+score, 
+countryName,
+customerType,
+customerStatus,
+'retently' as source
+ from {{ ref('feedbackM') }}
+union all
+select createdAt,
+score, 
+countryName,
+null,null,
+'yotpo' as source
+ from {{ ref('unifiedSiteReviews') }}
