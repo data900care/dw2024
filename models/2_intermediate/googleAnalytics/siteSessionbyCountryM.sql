404e96142f9ff90513f728cd685e34837c6a7c5b
Camille Capoul <data@900.care>
2024-03-20 10:04:17 +0000

    GA country report left join

--- models/2_intermediate/googleAnalytics/siteSessionbyCountryM.sql
+++ models/2_intermediate/googleAnalytics/siteSessionbyCountryM.sql
@@ -1,4 +1,4 @@
 select ga.date , ga.totalUsers, c.countryName, ga.conversions , ga.country
 from {{ ref('stg_externalData__ga4_demographic_country_report') }} ga
-join {{ ref('stg_BIContent900__content900_Country') }} c 
+left join {{ ref('stg_BIContent900__content900_Country') }} c 
 on c.GoogleAnalyticsName = ga.country
