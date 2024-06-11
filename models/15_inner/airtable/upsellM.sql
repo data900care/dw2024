8c84c84201cd2f700d6226e59e3ecee97e6c753f
Camille Capoul <data@900.care>
2024-06-11 14:00:54 +0000

    upsell Webbased with and without manual Upsell ga4 flattened event data exclude fivetran recharge models

--- models/15_inner/airtable/upsellM.sql
+++ models/15_inner/airtable/upsellM.sql
@@ -1,28 +1,53 @@
-with dedupAirtable as 
+with web0euroRechargeUpsell_WithoutManualUpsell as 
 (
-select  min(upsellId) as minUpsellId 
-from {{ ref("stg_airtable_upsell") }}
-group by subscriptionId
-),
-web0euroRechargeUpsell as 
-(
-select createdAt as dateUpsell, 'WebUpsell' as upsellType, 'web' as contactChannel,0 as subscriptionId,shopify_orderId,validorder
- from {{ ref('inner_shopify__order_line') }} 
- join {{ ref('inner_shopify__order') }} using(shopify_orderId)
+select so.createdAt as dateUpsell, 'WebUpsell' as upsellType, 'web' as contactChannel,rol.subscriptionId,so.validorder,so.shopify_orderId, sol.lineIndex, so.newOrderCustomerType
+ from {{ ref('inner_shopify__order_line') }} sol
+ join {{ ref('shopifyOrderL') }} so using(shopify_orderId)
+ left join {{ ref('stg_recharge__order_airbyte') }} ro using(shopify_orderId)
+ left join {{ ref('stg_recharge__order_line_item_airbyte') }} rol on ro.recharge_orderId = rol.recharge_orderId and rol.lineIndex = sol.lineIndex
  where productType = 'Recharge' and price = 0
- and shopify_orderId not in (select shopify_orderId from {{ ref('orderBundles_from_order_note_attribute') }})
+ and so.shopify_orderId not in (select shopify_orderId from {{ ref('orderBundles_from_order_note_attribute') }})
+ and validOrder is true
+ and  not exists (select 1 from {{ ref("upsell_airtable") }} where new_shopify_orderId = sol.shopify_orderId)
+),
+ web0euroRechargeUpsell_PlusManualUpsell as 
+(
+select so.createdAt as dateUpsell, 'WebUpsell+' as upsellType, 'web' as contactChannel,rol.subscriptionId,so.validorder,so.shopify_orderId, sol.lineIndex, so.newOrderCustomerType
+ from {{ ref('inner_shopify__order_line') }} sol
+ join {{ ref('shopifyOrderL') }} so using(shopify_orderId)
+ join {{ ref("upsell_airtable") }} ua on new_shopify_orderId = so.shopify_orderId
+ left join {{ ref('stg_recharge__order_airbyte') }} ro on ro.shopify_orderId = ua.original_shopify_orderId
+ left join {{ ref('stg_recharge__order_line_item_airbyte') }} rol on ro.recharge_orderId = rol.recharge_orderId and rol.sku = sol.sku
+ where productType = 'Recharge' and price = 0
+ and so.shopify_orderId not in (select shopify_orderId from {{ ref('orderBundles_from_order_note_attribute') }})
+ and validOrder is true
 )
 
 select dateUpsell,
         upsellType,
         contactChannel,
-        subscriptionId
- from {{ ref("stg_airtable_upsell") }}       
-where upsellId in (select minUpsellId from dedupAirtable )
+        subscriptionId,
+        new_shopify_orderId,
+        newOrderCustomerType
+ from {{ ref("upsell_airtable") }}   
+
 union all
+
 select dateUpsell,
         upsellType,
         contactChannel,
-        subscriptionId
-from web0euroRechargeUpsell
-where validOrder is true
+        subscriptionId,
+        shopify_orderId ,
+        newOrderCustomerType
+from web0euroRechargeUpsell_WithoutManualUpsell
+
+union all
+
+select dateUpsell,
+        upsellType,
+        contactChannel,
+        subscriptionId,
+        shopify_orderId ,
+        newOrderCustomerType
+from web0euroRechargeUpsell_PlusManualUpsell
+
