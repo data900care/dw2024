20e9b37a92da151351195bdac95b6e7daa7d0878
Camille Capoul <data@900.care>
2024-06-11 14:28:00 +0000

    dedup upsell by subscriptionID (weird case of subscriptionId = 423990802 with 3 upsells)

--- models/2_intermediate/subscription/subscriptionM.sql
+++ models/2_intermediate/subscription/subscriptionM.sql
@@ -1,5 +1,16 @@
+with dedupUpsell as 
+(
+select  min(new_shopify_orderId) as min_new_shopify_orderId 
+from {{ ref("upsellM") }}
+group by subscriptionId
+),
+upsell as 
+(select subscriptionId, upsellType 
+from {{ ref("upsellM") }}
+where new_shopify_orderId in (select min_new_shopify_orderId from dedupUpsell )
+)
+
 select s.*, u.upsellType
  from {{ ref('inner_recharge__subscription') }} s
-
-left join {{ ref('upsellM') }} u 
+left join upsell u 
     using(subscriptionId)
