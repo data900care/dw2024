d4a19382cd9fedf4bb5404b3fb7f07d0b05708dc
Côme Gilbert <data@900.care>
2024-10-03 09:27:31 +0000

    customer first sessions and session first not null attributes

--- models/2_intermediate/googleAnalytics/firstSession.sql
+++ models/2_intermediate/googleAnalytics/firstSession.sql
@@ -1,13 +1,57 @@
-with firstSessionId as
+with customer_first_sessionId as
 (SELECT customer_id, first_value(ga_session_id) OVER (PARTITION BY customer_id ORDER BY event_timestamp ASC ) as ga_session_id
 FROM {{ ref('stg_ga4_events') }}
 where customer_id is not null and ga_session_id is not null)
+,
+session_first_device_category as 
+(select ga_session_id, first_value(device_category) OVER (PARTITION BY ga_session_id ORDER BY event_timestamp ASC ) as device_category
+FROM {{ ref('stg_ga4_events') }}
+where customer_id is not null and ga_session_id is not null and device_category is not null)
+,
+session_first_device_mobile_brand_name as 
+(select ga_session_id, first_value(device_mobile_brand_name) OVER (PARTITION BY ga_session_id ORDER BY event_timestamp ASC ) as device_mobile_brand_name
+FROM {{ ref('stg_ga4_events') }}
+where customer_id is not null and ga_session_id is not null and device_mobile_brand_name is not null)
+,
+session_first_geo_country as 
+(select ga_session_id, first_value(geo_country) OVER (PARTITION BY ga_session_id ORDER BY event_timestamp ASC ) as geo_country
+FROM {{ ref('stg_ga4_events') }}
+where customer_id is not null and ga_session_id is not null and geo_country is not null)
+,
+session_first_traffic_source_source as 
+(select ga_session_id, first_value(traffic_source_source) OVER (PARTITION BY ga_session_id ORDER BY event_timestamp ASC ) as traffic_source_source
+FROM {{ ref('stg_ga4_events') }}
+where customer_id is not null and ga_session_id is not null and traffic_source_source is not null)
+,
+session_first_manual_source as 
+(select ga_session_id, first_value(manual_source) OVER (PARTITION BY ga_session_id ORDER BY event_timestamp ASC ) as manual_source
+FROM {{ ref('stg_ga4_events') }}
+where customer_id is not null and ga_session_id is not null and manual_source is not null)
+,
+session_first_manual_content as 
+(select ga_session_id, first_value(manual_source) OVER (PARTITION BY ga_session_id ORDER BY event_timestamp ASC ) as manual_content
+FROM {{ ref('stg_ga4_events') }}
+where customer_id is not null and ga_session_id is not null and manual_content is not null)
+,
+session_first_manual_campaign_name as 
+(select ga_session_id, first_value(manual_campaign_name) OVER (PARTITION BY ga_session_id ORDER BY event_timestamp ASC ) as manual_campaign_name
+FROM {{ ref('stg_ga4_events') }}
+where customer_id is not null and ga_session_id is not null and manual_campaign_name is not null)
 
-SELECT distinct customer_id,ga_session_id,device_category,device_mobile_brand_name,geo_country,traffic_source_source
+SELECT  customer_id,ga_session_id
+,device_category
+,device_mobile_brand_name
+,geo_country
+,traffic_source_source
 ,manual_source as UTM_source
 ,manual_content as UTM_content
 ,manual_campaign_name as UTM_campaign_name
-FROM {{ ref('stg_ga4_events') }}
-join firstSessionId using(customer_id,ga_session_id)
-where customer_id is not null and ga_session_id is not null
---order by user_id
+FROM customer_first_sessionId
+left join session_first_device_category using(ga_session_id)
+left join session_first_device_mobile_brand_name using(ga_session_id)
+left join session_first_geo_country using(ga_session_id)
+left join session_first_traffic_source_source using(ga_session_id)
+left join session_first_manual_source using(ga_session_id)
+left join session_first_manual_content using(ga_session_id)
+left join session_first_manual_campaign_name using(ga_session_id)
+
--- models/3_marts/googleAnalytics/events/ga4_session_summary.sql
+++ models/3_marts/googleAnalytics/events/ga4_session_summary.sql
@@ -1,7 +1,9 @@
 
  with
 filtered_events as 
-(select geo_country, ga_session_id, customer_id, REPLACE(replace(event_name,'-','_'), ' ','_') as event_name, event_timestamp
+(select geo_country, ga_session_id, 
+customer_id, user_id,
+REPLACE(replace(event_name,'-','_'), ' ','_') as event_name, event_timestamp
 FROM  {{ ref('stg_ga4_events') }}
 where event_name in
  ('session_start','GA4 - product_page_cta','add_to_cart','funnel_step_antichurn_cta','add_upsell_to_cart','begin_checkout','purchase')
@@ -10,8 +12,8 @@ where event_name in
 ),
 session_customer_id as 
 (
-  select distinct  geo_country,ga_session_id ,customer_id 
-  from filtered_events where ga_session_id is not null and customer_id is not null 
+  select distinct  geo_country,ga_session_id ,customer_id ,user_id,
+  from filtered_events where ga_session_id is not null and (customer_id is not null or user_id is not null)
 ),
  pivoted As 
 (SELECT * FROM (
@@ -25,6 +27,6 @@ FOR event_name IN ('session_start','GA4___product_page_cta','add_to_cart','funne
 )
 
 
-select  s.customer_id,p.*  from pivoted p
+select  s.customer_id,s.user_id, p.*  from pivoted p
 left join session_customer_id s using(ga_session_id)
 where first_session_start is not null-- and user_id is not null
